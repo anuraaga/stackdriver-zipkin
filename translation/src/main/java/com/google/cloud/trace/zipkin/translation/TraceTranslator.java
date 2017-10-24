6df8669cd5c2cf0da9e224dd6c81b1a596baeacf
Anuraag Agrawal <rag@istellar.jp>
2017-10-24 16:33:58 +0900

    Migrate business logic to use native zipkin2 API.

--- translation/src/main/java/com/google/cloud/trace/zipkin/translation/TraceTranslator.java
+++ translation/src/main/java/com/google/cloud/trace/zipkin/translation/TraceTranslator.java
@@ -17,18 +17,16 @@
 package com.google.cloud.trace.zipkin.translation;
 
 import com.google.devtools.cloudtrace.v1.Trace;
-import com.google.devtools.cloudtrace.v1.TraceSpan;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import zipkin.Span;
-import zipkin.internal.Span2;
-import zipkin.internal.Span2Converter;
+import zipkin2.Span;
 
 /**
- * TraceTranslator converts a collection of Zipkin Spans into a Collection of Stackdriver Trace Spans.
+ * TraceTranslator converts a collection of Zipkin Spans into a Collection of Stackdriver Trace
+ * Spans.
  */
 public class TraceTranslator {
   private final SpanTranslator translator;
@@ -36,7 +34,9 @@ public class TraceTranslator {
 
   /**
    * Create a TraceTranslator.
-   * @param projectId The Google Cloud Platform projectId that should be used for Stackdriver Traces.
+   *
+   * @param projectId The Google Cloud Platform projectId that should be used for Stackdriver
+   *     Traces.
    */
   public TraceTranslator(String projectId) {
     this.translator = new SpanTranslator();
@@ -45,6 +45,7 @@ public class TraceTranslator {
 
   /**
    * Convert a Collection of Zipkin Spans into a Collection of Stackdriver Trace Spans.
+   *
    * @param zipkinSpans The Collection of Zipkin Spans.
    * @return A Collection of Stackdriver Trace Spans.
    */
@@ -57,7 +58,7 @@ public class TraceTranslator {
       Span currentSpan = sortedByTraceAndSpanId.get(i);
 
       // Zipkin trace ID is conditionally 16 or 32 characters, but Stackdriver needs 32
-      String traceId = currentSpan.traceIdString();
+      String traceId = currentSpan.traceId();
       if (traceId.length() == 16) traceId = "0000000000000000" + traceId;
 
       if (currentTrace == null || !traceId.equals(currentTrace.getTraceId())) {
@@ -67,19 +68,6 @@ public class TraceTranslator {
         currentTrace.setProjectId(this.projectId);
         currentTrace.setTraceId(traceId);
       }
-
-      // Clients and servers may report against the same span ID. Collect all fragments together.
-      List<Span2> sameSpan = new ArrayList<>(Span2Converter.fromSpan(currentSpan));
-      while (i + 1 < length) {
-        Span peekedSpan = sortedByTraceAndSpanId.get(i + 1);
-        if (currentSpan.traceId != peekedSpan.traceId || currentSpan.id != peekedSpan.id) break;
-        sameSpan.addAll(Span2Converter.fromSpan(peekedSpan));
-        i++;
-      }
-
-      for (Span2 span2 : sameSpan) {
-        appendSpan(currentTrace, span2);
-      }
     }
     finishTrace(currentTrace, translatedTraces);
     return translatedTraces;
@@ -87,20 +75,17 @@ public class TraceTranslator {
 
   private List<Span> sortByTraceAndSpanId(Collection<Span> input) {
     List<Span> result = new ArrayList<>(input);
-    Collections.sort(result, new Comparator<Span>() {
-      @Override
-      public int compare(Span o1, Span o2) {
-        int result = Long.compare(o1.traceId, o2.traceId);
-        if (result != 0) return result;
-        return Long.compare(o1.id, o2.id);
-      }
-    });
+    Collections.sort(
+        result,
+        new Comparator<Span>() {
+          @Override
+          public int compare(Span o1, Span o2) {
+            int result = o1.traceId().compareTo(o2.traceId());
+            if (result != 0) return result;
+            return o1.id().compareTo(o2.id());
+          }
+        });
     return result;
-  }
-
-  private void appendSpan(Trace.Builder builder, Span2 zipkinSpan) {
-    TraceSpan span = translator.translate(TraceSpan.newBuilder(), zipkinSpan).build();
-    builder.addSpans(span);
   }
 
   private void finishTrace(Trace.Builder traceBuilder, Collection<Trace> convertedTraces) {
